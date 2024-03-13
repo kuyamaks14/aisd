c3c69967c596585e637feeeaadbc275eb7025a69
Ponomarev Maksim <maksim.1403@mail.ru>
2024-03-13 11:38:35 +0300

    update old version

--- lab_1/main.c
+++ lab_1/main.c
@@ -2,29 +2,31 @@
 #include "tools.h"
 
 int main(void) {
-    int m = getAmountOfRows();
-
-    int n[m];
-    getAmountOfColumns(m, n);
-
-    int **rowsPtrArr = fillMatrix(m, n);
-
-
-    int nCopy[m];
-    copyArrayOfLengths(m, nCopy, n);
-
-    int *rowsPtrArrCopy[m];
-    copyArrayOfPtrs(m, rowsPtrArrCopy, rowsPtrArr);
-
-    sortMatrixRows(m, nCopy, rowsPtrArrCopy);
-
-    puts("---Initial matrix---");
-    printMatrix(m, n, rowsPtrArr);
-
-    puts("\n---Sorted matrix---");
-    printMatrix(m, nCopy, rowsPtrArrCopy);
-
-    freeMatrix(m, n, rowsPtrArr);
+//    int m = getAmountOfRows();
+//
+//    int n[m];
+//    getAmountOfColumns(m, n);
+//
+//    int **rowsPtrArr = fillMatrix(m, n);
+//
+//
+//    int nCopy[m];
+//    copyArrayOfLengths(m, nCopy, n);
+//
+//    int *rowsPtrArrCopy[m];
+//    copyArrayOfPtrs(m, rowsPtrArrCopy, rowsPtrArr);
+//
+//    sortMatrixRows(m, nCopy, rowsPtrArrCopy);
+//
+//    puts("---Initial matrix---");
+//    printMatrix(m, n, rowsPtrArr);
+//
+//    puts("\n---Sorted matrix---");
+//    printMatrix(m, nCopy, rowsPtrArrCopy);
+//
+//    freeMatrix(m, n, rowsPtrArr);
+    struct Test t;
+    printf("%d\n", sizeof(t));
     return 0;
 }
 
